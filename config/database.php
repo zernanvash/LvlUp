870a6ad1198e978be511a07ae58a86ac53a1832c
zernanvash <zernanvasharive16@gmail.com>
2026-03-04 23:27:18 +0800

    some databse

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
